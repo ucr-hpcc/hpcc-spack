d8c31134f5b2dd6e6eae8da0d48c5d9542e28ff9
Emerson Jacobson <emwjacobson@gmail.com>
2026-02-05 13:32:47 -0800

    Added itpp

---
+++ spack_repo/hpcc_pkgs/packages/itpp/package.py
@@ -0,0 +1,31 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+
+class Itpp(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+    url = "https://sourceforge.net/projects/itpp/files/itpp-4.3.1.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers("github_user1", "github_user2")
+
+    # FIXME: Add the SPDX identifier of the project's license below.
+    # See https://spdx.org/licenses/ for a list. Upon manually verifying
+    # the license, set checked_by to your Github username.
+    license("UNKNOWN", checked_by="github_user1")
+
+    version("4.3.1", sha256="15333863c837dba1a0b3e6e84b5a2394e44bb62e3b751521b94f9b65bbf0ad91")
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("blas")
+
