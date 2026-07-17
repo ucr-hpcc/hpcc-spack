1eb319d6038a58790028c5d9d72e61c3d5e7c988
Emerson Jacobson <emwjacobson@gmail.com>
2026-07-17 11:32:39 -0700

    Added gcc conflict for vcflib

--- spack_repo/hpcc_pkgs/packages/vcflib/package.py
+++ spack_repo/hpcc_pkgs/packages/vcflib/package.py
@@ -17,6 +17,8 @@ class Vcflib(CMakePackage):
     version("1.0.15", sha256="178e8c27fffc5324ac73f1c4b35f407184271b57f82aedc2efb9703df6ee3d49")
     version("1.0.14", sha256="27ba26a3c48ba3911e760de1c5633d46b57e1e8dce9ad41e8cb3de299d2d6053")
 
+    conflicts("gcc@:11")
+
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("zig@0.13", when="@1.0.14")
