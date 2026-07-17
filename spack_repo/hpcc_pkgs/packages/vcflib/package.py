c6bca293b8ca5caf71f6096107c2f840adc7328b
Emerson Jacobson <emwjacobson@gmail.com>
2026-07-17 11:32:09 -0700

    Updated vcflib

--- spack_repo/hpcc_pkgs/packages/vcflib/package.py
+++ spack_repo/hpcc_pkgs/packages/vcflib/package.py
@@ -14,11 +14,13 @@ class Vcflib(CMakePackage):
     homepage = "https://www.example.com"
     url = "https://github.com/vcflib/vcflib/releases/download/v1.0.14/vcflib-1.0.14-src.tar.gz"
 
+    version("1.0.15", sha256="178e8c27fffc5324ac73f1c4b35f407184271b57f82aedc2efb9703df6ee3d49")
     version("1.0.14", sha256="27ba26a3c48ba3911e760de1c5633d46b57e1e8dce9ad41e8cb3de299d2d6053")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
-    depends_on("zig@0.13")
+    depends_on("zig@0.13", when="@1.0.14")
+    depends_on("zig@0.15", when="@1.0.15")
     depends_on("py-pybind11")
 
     depends_on("htslib")
